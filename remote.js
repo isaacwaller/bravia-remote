5ab7886763e66f4fe29a9f67c46c1f85bd1cd007
Isaac Waller <isaac@siriusapplications.com>
2016-06-13 16:10:25 -0700

    fix cookie

--- remote.js
+++ remote.js
@@ -1,9 +1,15 @@
 var bravia = require('bravia')('192.168.1.106');
-var electron = require('electron');
-var app = electron.app;
-var globalShortcut = electron.globalShortcut;
+
+var useElectron = true;
 
 bravia.mac = "D8:D4:3C:A9:97:78";
+
+// Hardcode cookie
+var request = require('request');
+var jar = request.jar();
+var cookie = request.cookie("auth=0c7e6a4582e17bbdb6b0e74678611c9ee9535a9e8065d2b2c59f1a83e95bb274");
+jar.setCookie(cookie, 'http://192.168.1.106');
+bravia.cookieJar = jar;
 
 var lastPressTime = new Date().getTime();
 function pressHandler(command) {
@@ -12,6 +18,15 @@ function pressHandler(command) {
         lastPressTime = new Date().getTime();
     }
 }
+
+if (!useElectron) {
+  bravia.exec('VolumeUp');
+  return;
+}
+
+var electron = require('electron');
+var app = electron.app;
+var globalShortcut = electron.globalShortcut;
 
 app.on('ready', () => {
   var up = globalShortcut.register('VolumeUp', () => {
